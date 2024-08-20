227b4fb19a814ab7e6f2f15be5b18c21fa264cd7
ac618 <andychou618@gmail.com>
2024-08-20 23:21:04 +0800

    Audio Pass Through with Mic

---
+++ STM32F746G-DISCO/AudioPassThrough/Core/Src/main.c
@@ -0,0 +1,286 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "sai.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "stm32746g_discovery_audio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+// Printf
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+// Buffer
+#define PCM_BUFFER_SIZE 8192
+// Flags
+#define BUFFER_NONE 0
+#define BUFFER_HALF_FULL 1
+#define BUFFER_FULL 2
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+// Buffers
+uint16_t PlayBuffer[PCM_BUFFER_SIZE];
+uint16_t RecordBuffer[PCM_BUFFER_SIZE];
+// Flags
+uint8_t BUFFER_STATE = BUFFER_NONE;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void PeriphCommonClock_Config(void);
+/* USER CODE BEGIN PFP */
+void CopyBuffer(uint16_t *dest, uint16_t *orig, size_t size);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  // Generate Square Wave
+  // int frequency = 440;
+  // int samplingRate = 48000;
+  // int samplesPerPeriod = samplingRate / frequency;
+  // for (size_t i = 0; i < PCM_BUFFER_SIZE; i++)
+  // {
+  //   if ((i % samplesPerPeriod) < samplesPerPeriod / 2)
+  //   {
+  //     PlayBuffer[i] = 500;
+  //   }
+  //   else
+  //   {
+  //     PlayBuffer[i] = 0;
+  //   }
+  // }
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+/* Configure the peripherals common clocks */
+  PeriphCommonClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_SAI2_Init();
+  MX_TIM1_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+  BSP_AUDIO_IN_OUT_Init(INPUT_DEVICE_DIGITAL_MICROPHONE_2, OUTPUT_DEVICE_HEADPHONE, AUDIO_FREQUENCY_48K, DEFAULT_AUDIO_IN_BIT_RESOLUTION, 2);
+
+  BSP_AUDIO_IN_SetVolume(20);
+  BSP_AUDIO_IN_Record(RecordBuffer, PCM_BUFFER_SIZE);
+
+  BSP_AUDIO_OUT_SetVolume(60);
+  BSP_AUDIO_OUT_Play(PlayBuffer, PCM_BUFFER_SIZE * 2);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    if (BUFFER_STATE == BUFFER_HALF_FULL)
+    {
+      CopyBuffer(PlayBuffer, RecordBuffer, PCM_BUFFER_SIZE / 2);
+      BUFFER_STATE = BUFFER_NONE;
+    }
+    else if (BUFFER_STATE == BUFFER_FULL)
+    {
+      CopyBuffer(PlayBuffer + PCM_BUFFER_SIZE / 2, RecordBuffer + PCM_BUFFER_SIZE / 2, PCM_BUFFER_SIZE / 2);
+      BUFFER_STATE = BUFFER_NONE;
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 10;
+  RCC_OscInitStruct.PLL.PLLN = 210;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Peripherals Common Clock Configuration
+  * @retval None
+  */
+void PeriphCommonClock_Config(void)
+{
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Initializes the peripherals clock
+  */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
+  PeriphClkInitStruct.PLLSAI.PLLSAIR = 2;
+  PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
+  PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV2;
+  PeriphClkInitStruct.PLLSAIDivQ = 1;
+  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_2;
+  PeriphClkInitStruct.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+PUTCHAR_PROTOTYPE
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
+}
+void CopyBuffer(uint16_t *dest, uint16_t *orig, size_t size)
+{
+  for (size_t i = 0; i < size; i++)
+  {
+    dest[i] = orig[i];
+  }
+}
+void BSP_AUDIO_IN_HalfTransfer_CallBack()
+{
+  BUFFER_STATE = BUFFER_HALF_FULL;
+}
+void BSP_AUDIO_IN_TransferComplete_CallBack()
+{
+  BUFFER_STATE = BUFFER_FULL;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ STM32F746G-DISCO/AudioPassThrough/test.py
@@ -0,0 +1,29 @@
+import numpy as np
+import wave
+
+# Parameters
+f = 440.0  # Frequency of the square wave in Hz
+Fs = 48000  # Sampling rate in Hz
+T = 1.0    # Duration in seconds
+A = 0.1    # Amplitude of the square wave
+
+# Number of samples
+N = int(Fs * T)
+
+# Time vector
+t = np.arange(N) / Fs
+
+# Generate the square wave
+square_wave = A * np.sign(np.sin(2 * np.pi * f * t))
+
+# Convert to 16-bit PCM
+pcm_wave = np.int16(32767 * square_wave)
+
+print(pcm_wave)
+
+# Save to a WAV file
+with wave.open('square_wave.wav', 'w') as wf:
+    wf.setnchannels(1)  # Mono
+    wf.setsampwidth(2)  # 16-bit
+    wf.setframerate(Fs)
+    wf.writeframes(pcm_wave.tobytes())
